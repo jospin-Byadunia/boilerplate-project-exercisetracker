8cb4cbb1aea62dd08134248fa390a8c44dd6c373
jospin murhiorhakube <jmurhiorhakube@gmail.com>
2024-07-01 07:06:59 +0000

    connecting to the database and serer listenning in the server.js file

---
+++ server.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const app = require('./index');
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+
+
+const DB = process.env.DATABASE.replace(
+    "<PASSWORD>",
+    process.env.DATABASE_PASSWORD
+  );
+  
+mongoose.connect(DB).then(() => console.log("DB connection successful")).catch((err)=>{
+    console.log(`Error connecting to the DB`);
+});
+
+const listener = app.listen(process.env.PORT || 3000, () => {
+    console.log('Your app is listening on port ' + listener.address().port)
+  })
