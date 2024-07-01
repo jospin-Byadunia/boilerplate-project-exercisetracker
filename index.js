f0bf2e78faefb5348520f5b370029eae849e208b
jospin murhiorhakube <jmurhiorhakube@gmail.com>
2024-07-01 07:03:42 +0000

    configured the environment and included middleware body-parser

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
-require('dotenv').config()
+const router = require('./routes/userExerciseRoute');
+const bodyParser = require('body-parser');
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
 
 
+app.use('/api/users', router);
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+
+module.exports=app
